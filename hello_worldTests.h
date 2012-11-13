2333fcf72ddee83a270e12e26bd1fc7da8f49db8
Laurent 2 <laurent1jacquot@gmail.com>
2012-11-12 17:11:56 -0800

    First commit

---
+++ hello_worldTests.h
@@ -0,0 +1,13 @@
+//
+//  hello_worldTests.h
+//  hello worldTests
+//
+//  Created by App Jam on 10/29/12.
+//  Copyright (c) 2012 App Jam. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface hello_worldTests : SenTestCase
+
+@end
