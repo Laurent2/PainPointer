5bd32b021ada2aa7c5636a76a57c0863a93c85f8
Eric <erictsayswhat@gmail.com>
2012-11-19 00:34:42 -0800

    Revert "lo"
    This reverts commit 946213a0822d31ca8fe808ccf99124bd71c909d1.

---
+++ PainPointer/PainPointer/imageCell.h
@@ -0,0 +1,13 @@
+//
+//  imageCell.h
+//  PainPointer
+//
+//  Created by Justin Phan on 11/18/12.
+//  Copyright (c) 2012 App Jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface imageCell : UITableViewCell
+
+@end
